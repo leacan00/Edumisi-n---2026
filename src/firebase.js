a13b1f64b60d16b92d2b8faba2d8ecffdec77075
leacan00 <leacan00@gmail.com>
2026-09-25 12:56:21 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY_AQUI",
+  authDomain: "edumision-2026.firebaseapp.com",
+  projectId: "edumision-2026",
+  storageBucket: "edumision-2026.appspot.com",
+  messagingSenderId: "TU_SENDER_ID_AQUI",
+  appId: "TU_APP_ID_AQUI"
+};
